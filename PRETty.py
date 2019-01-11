adcacdcbb33b1f01d6faf3da2918b5d389598197
BusesCanFly <vanya.gorbachev@gmail.com>
2019-01-11 07:36:56 -0500

    Reworked arp-scanning

--- PRETty.py
+++ PRETty.py
@@ -26,28 +26,30 @@ cprint("Step 2: Select IP list", 'yellow')
 cprint("Step 3: Select PRET command input file", 'yellow')
 cprint("Step 4: Select shell type", 'yellow')
 cprint("Step 5: Observe all laws and ethical/moral codes :D", 'yellow')
-cprint("Step 6: >:)", 'yellow')
+cprint("Step 6: >:)\n", 'yellow')
 
 gen_new= str(raw_input("Generate new IP list? [y/N] "))
 if gen_new == str('y'):
-	interface= raw_input("Wireless interface name: [ex. wlan0/wlp58s0] ")
 
 	set_ip_range = raw_input("Set IP range for scanning? [y/N] ")
 	if set_ip_range == str('y'):
-		ip_range = str("-r ") + str(raw_input("IP range: [ex. 192.168.0.0/16] "))
+		ip_range = str(raw_input("IP range: [ex. 192.168.0.0/16] "))
 	else:
-		ip_range = ''
-	cprint("Ctrl+C after 10-30 seconds to finish scanning (more time for larger networks)", 'green')
-        sleep(2.5)
-	os.system('sudo netdiscover ' + ip_range + ' -i '+ interface +' '+ ' | grep -wi \'Hewlett\|Brother\|Kyocera\|Laserjet\' > ./IP/list')
-	cprint('Received '+ str(sum(1 for line in open ('./IP/list'))) + ' ARP requests', 'yellow')
-	sleep(1.5)
-	cprint('Successfully collected IP\'s', 'green')
-	os.system('awk \'{print $1}\' ./IP/list | sort -u > ./IP/HP_list')
-	sleep(1.5)
-        cprint('Successfully processed raw data', 'green')
-        os.system('rm ./IP/list')
+		ip_range = '--localnet'
+
+	cprint("ARP scanning LAN for devices...", 'green')
         sleep(1.5)
+	os.system('sudo arp-scan -g '+ip_range+' -W ./IP/scan.pcap')
+	cprint('Successfully collected IP\'s', 'green')
+	os.system('tshark -r ./IP/scan.pcap > ./IP/pcap.txt')
+	os.system('cat ./IP/pcap.txt | grep -iE "Hewlett|Brother|Kyocera|Laserjet" > ./IP/printer_list')
+	os.system('awk \'{print $8}\' ./IP/printer_list > ./IP/HP_list')
+	sleep(1.5)
+	cprint('Successfully processed raw data', 'green')
+        os.system('rm -rf ./IP/scan.pcap && rm -rf ./IP/pcap.txt && rm -rf ./IP/printer_list')
+	sleep(1.5)
+	cprint('Cleaned raw data', 'green')
+	sleep(1.5)
 	cprint('\nLocated '+ str(sum(1 for line in open ('./IP/HP_list'))) +' printers, storing as ./IP/HP_list\n', 'yellow')
 
 list_answer = str(raw_input("Use default IP list? [Y/n] "))
@@ -90,5 +92,4 @@ with open(list) as inf:
 i=0
 while i < len(lines):
 	os.system('../pret.py '+debug_enabled+' -i '+commands_list+' -q '+lines[i]+' '+ shell_type)
-	#print('../pret.py '+debug_enabled+' -i '+commands_list+' -q '+lines[i]+' '+ shell_type)
 	i+=1
