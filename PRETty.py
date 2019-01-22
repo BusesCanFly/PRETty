d78a288fc50b26e620c52f5029d6be375d7c825f
BusesCanFly <vanya.gorbachev@gmail.com>
2019-01-22 12:33:25 -0500

    ASCII art fun

--- PRETty.py
+++ PRETty.py
@@ -4,31 +4,37 @@ import os
 import argparse
 from time import sleep
 from termcolor import colored, cprint
-main_color = str('green')
-sub_color = str('blue')
-line_color = str('red')
+main_color = 'green'
+sub_color = 'blue'
+line_color = 'red'
+text_color = 'white'
+alt_text = ' automation tool'
 
-cprint(" ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄         ▄  ", main_color)
-cprint("▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌ ", main_color)
-cprint("▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀  ▀▀▀▀█░█▀▀▀▀  ▀▀▀▀█░█▀▀▀▀ ▐░▌       ▐░▌ ", main_color)
-cprint("▐░▌       ▐░▌▐░▌       ▐░▌▐░▌               ▐░▌          ▐░▌     ▐░▌       ▐░▌ ", main_color)
-cprint("▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄      ▐░▌          ▐░▌     ▐░█▄▄▄▄▄▄▄█░▌ ", main_color)
-cprint("▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░▌          ▐░▌     ▐░░░░░░░░░░░▌ ", main_color)
-cprint("▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀█░█▀▀ ▐░█▀▀▀▀▀▀▀▀▀      ▐░▌          ▐░▌      ▀▀▀▀█░█▀▀▀▀  ", main_color)
-cprint("▐░▌          ▐░▌     ▐░▌  ▐░▌               ▐░▌          ▐░▌          ▐░▌      ", main_color)
-cprint("▐░▌          ▐░▌      ▐░▌ ▐░█▄▄▄▄▄▄▄▄▄      ▐░▌          ▐░▌          ▐░▌      ", main_color)
-cprint("▐░▌          ▐░▌       ▐░▌▐░░░░░░░░░░░▌     ▐░▌          ▐░▌          ▐░▌      ", main_color)
-cprint(" ▀            ▀         ▀  ▀▀▀▀▀▀▀▀▀▀▀       ▀            ▀            ▀       ", main_color)
-cprint("  BusesCanFly   		  			    76 33 2e 30        ", sub_color)
-cprint("-------------------------------------------------------------------------------", line_color)
-cprint("              \"PRinter Exploitation Toolkit\" LAN automation tool             ", 'white')
-cprint("-------------------------------------------------------------------------------", line_color)
-cprint("Step 1: Generate IP list", 'yellow')
-cprint("Step 2: Select IP list", 'yellow')
-cprint("Step 3: Select PRET command input file", 'yellow')
-cprint("Step 4: Select shell type", 'yellow')
-cprint("Step 5: Observe all laws and ethical/moral codes :D", 'yellow')
-cprint("Step 6: >:)\n", 'yellow')
+def main_text():
+	cprint("              \"PRinter Exploitation Toolkit\" LAN"+alt_text+"             ", text_color)
+def main_art():
+	cprint(" ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄         ▄  ", main_color)
+	cprint("▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌ ", main_color)
+	cprint("▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀  ▀▀▀▀█░█▀▀▀▀  ▀▀▀▀█░█▀▀▀▀ ▐░▌       ▐░▌ ", main_color)
+	cprint("▐░▌       ▐░▌▐░▌       ▐░▌▐░▌               ▐░▌          ▐░▌     ▐░▌       ▐░▌ ", main_color)
+	cprint("▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄      ▐░▌          ▐░▌     ▐░█▄▄▄▄▄▄▄█░▌ ", main_color)
+	cprint("▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░▌          ▐░▌     ▐░░░░░░░░░░░▌ ", main_color)
+	cprint("▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀█░█▀▀ ▐░█▀▀▀▀▀▀▀▀▀      ▐░▌          ▐░▌      ▀▀▀▀█░█▀▀▀▀  ", main_color)
+	cprint("▐░▌          ▐░▌     ▐░▌  ▐░▌               ▐░▌          ▐░▌          ▐░▌      ", main_color)
+	cprint("▐░▌          ▐░▌      ▐░▌ ▐░█▄▄▄▄▄▄▄▄▄      ▐░▌          ▐░▌          ▐░▌      ", main_color)
+	cprint("▐░▌          ▐░▌       ▐░▌▐░░░░░░░░░░░▌     ▐░▌          ▐░▌          ▐░▌      ", main_color)
+	cprint(" ▀            ▀         ▀  ▀▀▀▀▀▀▀▀▀▀▀       ▀            ▀            ▀       ", main_color)
+	cprint("  BusesCanFly   		  			    76 33 2e 30        ", sub_color)
+	cprint("-------------------------------------------------------------------------------", line_color)
+	main_text()
+	cprint("-------------------------------------------------------------------------------", line_color)
+def interactive_steps():
+	cprint("Step 1: Generate IP list", 'yellow')
+	cprint("Step 2: Select IP list", 'yellow')
+	cprint("Step 3: Select PRET command input file", 'yellow')
+	cprint("Step 4: Select shell type", 'yellow')
+	cprint("Step 5: Observe all laws and ethical/moral codes :D", 'yellow')
+	cprint("Step 6: >:)\n", 'yellow')
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--cli', dest='cli', action='store_true',
@@ -44,19 +50,18 @@ args = parser.parse_args()
 
 sleep_time=1.5
 def PrinterLogSort():
-	os.system('tshark -r ./IP/scan.pcap > ./IP/pcap.txt')
-        os.system('cat ./IP/pcap.txt | grep -iE "Hewlett|Brother|Kyocera|Laserjet" > ./IP/raw_list')
+	os.system('tshark -r ./IP/scan.pcap > ./IP/pcap.txt 2>/dev/null')
+        os.system('cat ./IP/pcap.txt | grep -iE "Hewlett|Brother|Kyocera|Laserjet" > ./IP/raw_list 2>/dev/null')
         os.system('awk \'{print $8}\' ./IP/raw_list > ./IP/Printer_list')
-        sleep(sleep_time)
-        cprint('Successfully processed raw data', 'green')
-        os.system('rm -rf ./IP/scan.pcap && rm -rf ./IP/pcap.txt && rm -rf ./IP/raw_list')
+        sleep(0.5)
+	cprint('Successfully processed raw data', 'green')
+        os.system('rm -rf ./IP/scan.pcap && rm -rf ./IP/pcap.txt && rm -rf ./IP/raw_list 2>/dev/null')
         sleep(sleep_time)
         cprint('Cleaned raw data', 'green')
         sleep(sleep_time)
         cprint('\nLocated '+ str(sum(1 for line in open ('./IP/Printer_list'))) +' printers, storing as ./IP/Printer_list\n', 'yellow')
 
 def PRETty_Interactive():
-	sleep_time=1.5
 	gen_new= str(raw_input("Generate new IP list? [y/N] "))
 	if gen_new == str('y'):
 
@@ -120,7 +125,6 @@ def PRETty_Interactive():
                 i+=1
 
 def PRETty_cli():
-	sleep_time=0.5
         os.system('sudo arp-scan -g '+args.ip_range+' -W ./IP/scan.pcap')
 	PrinterLogSort()
 	sleep(1)
@@ -133,6 +137,18 @@ def PRETty_cli():
                 i+=1
 
 if args.cli:
+        main_color = 'red'
+        sub_color = 'green'
+        line_color = 'white'
+	text_color = 'yellow'
+	alt_text =' AUTOPWN      '
+        main_art()
+	sleep_time=0.5
 	PRETty_cli()
 else:
+	alt_text = ' automation tool'
+	main_art()
+        interactive_steps()
+	sleep_time=1.5
 	PRETty_Interactive()
+
